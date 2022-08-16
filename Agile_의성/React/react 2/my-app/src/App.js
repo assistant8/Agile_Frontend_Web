ac40286866c2999f473dbfc26ad0170cd0c43f1f
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-17 02:55:11 +0900

    [0817]react

--- Agile_의성/React/react 2/my-app/src/App.js
+++ Agile_의성/React/react 2/my-app/src/App.js
@@ -1,21 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Movie from "./Component/Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
-      ` https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, [loading, movies]);
-
-  return <div>{loading ? <h1>{movies.}</h1> : null} </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
---
+++ Agile_의성/React/react 2/my-app/src/components/Movie.js
@@ -0,0 +1,30 @@
+import App from "../App";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: propTypes.number.isRequired,
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default Movie;
